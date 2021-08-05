3277cde2e7e7ef88d60a398c0a7199f97c12e271
othongonzaga <othoncesar1@hotmail.com>
2021-08-05 15:11:16 -0300

    ex011

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do carro é: ${vel}Km/h`)
+if(vel >60){
+    console.log('Você ultrapassou a velocidade. Foi multado!')
+}
+console.log('Dirija sempre com cinto de segurança')

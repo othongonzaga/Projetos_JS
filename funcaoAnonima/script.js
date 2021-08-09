6f3f71f3aa84a84ad70e96e6d7d668fa72bd45c6
othongonzaga <othoncesar1@hotmail.com>
2021-08-09 15:28:52 -0300

    funcaoAnonima

---
+++ funcaoAnonima/script.js
@@ -0,0 +1,19 @@
+function adicionar(...numeros){
+    let total = numeros.reduce(function(toal, proximo) {
+
+    })
+    console.log(total)
+}
+
+adicionar(1, 2, 3, 4, 5)
+
+//-------------------------------------------
+
+function adicionar(...numeros){
+    let total = numeros.reduce((toal, proximo) => {
+
+    })
+    console.log(total)
+}
+
+adicionar(1, 2, 3, 4, 5)  

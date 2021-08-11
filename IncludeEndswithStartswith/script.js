1e57ee014e023095d0ef29db2a4d7b0ee7dd9c1c
othongonzaga <othoncesar1@hotmail.com>
2021-08-11 11:02:31 -0300

    IncludeEndswithStartSwith

---
+++ IncludeEndswithStartswith/script.js
@@ -0,0 +1,24 @@
+//includes
+//endWith
+//startsWith
+
+let nomes = ['Mateus', 'Lucas', 'Othon']
+ console.log(nome.includes('Lucas'))
+
+ if(nomes.includes('Othon')){
+     console.log('Othon esta na lista')
+ }else{
+     console.log('Othon não está na lista')
+ }
+
+ //----------------------------------------------
+
+let nome = 'Othon'
+
+console.log(nome.endsWith('on'))
+
+//---------------------------------------------
+
+let nome = 'Luí'
+
+console.log(nome.startsWith('L'))

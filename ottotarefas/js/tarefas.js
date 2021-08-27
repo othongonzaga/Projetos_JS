915f1d64b4a12a421d629758db2f7d99ae336ace
othongonzaga <othoncesar1@hotmail.com>
2021-08-27 17:05:56 -0300

    melhorando o js

--- ottotarefas/js/tarefas.js
+++ ottotarefas/js/tarefas.js
@@ -6,11 +6,52 @@ inputNovaTarefa.addEventListener('keypress', (e) => {
     if(e.keyCode == 13){
         let tarefa = {
             nome: inputNovaTarefa.value,
-            id:'',
+            id:gerarId(),
         }
+
+        adicionarTarefa(tarefa)
     }
 })
 
+btnAddTarefa.addEventListener('click', (e) => {
+    let tarefa = {
+        nome: inputNovaTarefa.value,
+        id:gerarId(),
+    }
+
+    adicionarTarefa(tarefa)
+})
+
 function gerarId(){
-    return
+    return Math.floor(Math.random() * 3000)
+}
+
+function adicionarTarefa(tarefa){
+    let li = criarTagLI(tarefa)
+    listaTarefas.appendChild(li)
+    inputNovaTarefa.value = ''
+}
+
+function criarTagLI(tarefa){
+    let li = document.createElement('li')
+    let span = document.createElement('span')
+    span.classList.add('textoTarefa')
+
+    let div = document.createElement('div')
+    let btnEditar = document.createElement('button')
+    btnEditar.classList.add('btnAcao')
+    btnEditar.innerHTML= '<i class="fa fa-pencil"></i>'
+
+    let btnExcluir = document.createElement('button')
+    btnExcluir.classList.add('btnAcao')
+    btnExcluir.innerHTML= '<i class="fa fa-trash"></i>'
+
+    div.appendChild(btnEditar)
+    div.appendChild(btnExcluir)
+
+    li.appendChild(span)
+    li.appendChild(div)
+    return li
+
+
 }

a5e0cb179ce860dc8dcae8e3534f4c0976bdeaae
othongonzaga <othoncesar1@hotmail.com>
2021-09-09 23:19:57 -0300

    Fazendo a função listarTweet e mudando algumas coisas no html

--- ottitter/assets/js/script.js
+++ ottitter/assets/js/script.js
@@ -1,5 +1,6 @@
 const textarea = document.querySelector('textarea')
 const tweetar = document.querySelector('button')
+const feed = document.querySelector('.conteudoPrincipal__listaTweets')
 
 function pegarTweet(event){
     event.preventDefault()
@@ -16,9 +17,39 @@ function criarTweet(tweetTexto){
 
     const tweet ={
         nome: 'Douglas',
-        foto:'./src/img/ProfilePic(2).png',
+        foto:'.assets/src/img/ProfilePic.png',
         usuario: '@douglasNunes',
         texto: tweetTexto,
         tempo: `${hora}:${minutos}`
     }
+
+    listarTemplateTweet(tweet)
+}
+
+function listarTemplateTweet(tweet){
+    const {nome, foto, usuario, texto, tempo} = tweet
+
+    let li = document.createElement('li')
+    li.classList.add('conteudoPrincipal__tweet')
+    let img = document.createElement('img')
+    img.src = foto
+    img.classList.add('tweet__conteudo')
+
+    let div = document.createElement('div')
+    let h2 = document.createElement('h2')
+    h2.innerText = nome
+
+    let span = document.createElement('span')
+    span.innerText = `${usuario} ${tempo}`
+
+    let p = document.createElement('p')
+    p.innerText = texto
+
+    div.appendChild(h2)
+    div.appendChild(span)
+    div.appendChild(p)
+    li.appendChild(img)
+    li.appendChild(div)
+    feed.appendChild(li)
+
 }

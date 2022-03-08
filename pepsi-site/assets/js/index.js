8210373331bee7c4c0a2092eeb262fc3c6dd4866
othongonzaga <othoncesar1@hotmail.com>
2022-03-08 18:05:12 -0300

    Fazendo a parte JS do site

---
+++ pepsi-site/assets/js/index.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}

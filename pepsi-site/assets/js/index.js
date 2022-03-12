b794f06b7bdcd85502abeba9ebc03c67e855172e
othongonzaga <othoncesar1@hotmail.com>
2022-03-12 19:20:39 -0300

    Fazendo a parte da reponsividade

--- pepsi-site/assets/js/index.js
+++ pepsi-site/assets/js/index.js
@@ -6,3 +6,10 @@ function changeBgColor(color){
     const sec = document.querySelector('.sec');
     sec.style.background = color;
 }
+
+function menuToggle(){
+    const toggleMenu = document.querySelector('.toggleMenu')
+    const navigation = document.querySelector('.navigation')
+    toggleMenu.classList.toggle('active')
+    navigation.classList.toggle('active')
+}

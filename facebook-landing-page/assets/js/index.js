f13885af252c2632e344501df55bb184b5b0416d
othongonzaga <othoncesar1@hotmail.com>
2022-03-26 18:05:25 -0300

    Terminado a parte Css e html do site, e fazendo a parte js

---
+++ facebook-landing-page/assets/js/index.js
@@ -0,0 +1,20 @@
+const slides = document.querySelectorAll('.slides')
+const dots = document.querySelectorAll('.dot')
+
+function SetActive(i){
+    for(slide of slides){
+        slide.classList.remove('active')
+        slides[i].classList.add('active')
+    }
+
+    for(dot of dots){
+        dot.classList.remove('active')
+        dots[i].classList.add('active')
+    }
+}
+
+for (let j = 0; j<dots.length; j++){
+    dots[j].addEventListener('click', function(){
+        SetActive(j)
+    })
+}

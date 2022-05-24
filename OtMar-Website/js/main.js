3057f67c61053a323636c85c4491b2af5b5dd2e1
othongonzaga <othoncesar1@hotmail.com>
2022-05-24 19:40:48 -0300

    Fazendo o Scroll Active

--- OtMar-Website/js/main.js
+++ OtMar-Website/js/main.js
@@ -5,6 +5,7 @@ const navLink = document.querySelectorAll('.nav__link')
 const videoFile = document.getElementById('video-file'),
       videoButton = document.getElementById('video-button'),
       videoIcon = document.getElementById('video-icon')
+const sections = document.querySelectorAll('section[id]')      
 
 if(navAlternancia){
     navAlternancia.addEventListener('click', () =>{
@@ -74,3 +75,20 @@ function scrollUp(){
     if(this.scrollY > 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
